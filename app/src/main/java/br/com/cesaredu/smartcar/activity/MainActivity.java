4ab3b6346e2674a7499064875ae83b1696c1030b
Leonardo Trindade dos Santos Souza <leonardo-trindade@live.com>
2017-11-09 01:22:26 -0300

    implementing joystick

--- app/src/main/java/br/com/cesaredu/smartcar/activity/MainActivity.java
+++ app/src/main/java/br/com/cesaredu/smartcar/activity/MainActivity.java
@@ -6,25 +6,25 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
-import com.erz.joysticklibrary.JoyStick;
+import br.com.cesaredu.smartcar.utils.JoyStick;
 
 import at.abraxas.amarino.Amarino;
 import br.com.cesaredu.smartcar.R;
-import br.com.cesaredu.smartcar.game.GameView;
 
 /**
  * Created by CassioPaixao on 25/10/2017.
  */
 public class MainActivity extends AppCompatActivity implements JoyStick.JoyStickListener, View.OnTouchListener {
-    private GameView gameView;
-    private TextView textView;
     public static final String BLUETOOTH_MAC_ADDRESS = "20:16:10:24:30:84";
+    private TextView textView;
+    private int speed = 0;
+    private int direction = -1;
+    private  int servo_pos = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gameView = (GameView) findViewById(R.id.game);
         textView = (TextView) findViewById(R.id.textView);
         findViewById(R.id.btn_a).setOnTouchListener(this);
         findViewById(R.id.btn_b).setOnTouchListener(this);
@@ -33,27 +33,33 @@ public class MainActivity extends AppCompatActivity implements JoyStick.JoyStick
         findViewById(R.id.btn_r1).setOnTouchListener(this);
         findViewById(R.id.btn_x).setOnTouchListener(this);
         findViewById(R.id.btn_y).setOnTouchListener(this);
-        getObject();
+        JoyStick joyMove = (JoyStick) findViewById(R.id.joy1);
+        joyMove.setListener(this);
+        joyMove.setPadBackground(R.drawable.pad);
+        joyMove.setButtonDrawable(R.drawable.button);
+        joyMove.setType(JoyStick.TYPE_4_AXIS);
+
+        JoyStick joyRotate = (JoyStick) findViewById(R.id.joy2);
+        joyRotate.setListener(this);
+        joyRotate.setPadBackground(R.drawable.pad);
+        joyRotate.setButtonDrawable(R.drawable.button);
+        joyRotate.setType(JoyStick.TYPE_2_AXIS_LEFT_RIGHT);
+        joyRotate.enableStayPut(true);
     }
 
     @Override
-    public void onMove(JoyStick joyStick, double angle, double power, int direction) {
+    public void onMove(JoyStick joyStick, int speed, int direction) {
         switch (joyStick.getId()) {
             case R.id.joy1:
-                gameView.move(angle, power);
-                textView.setText("Forca: " + power + "\nDirecao: " + direction);
+                this.speed = speed;
+                this.direction = direction;
+                textView.setText("Velocidade: " + speed + "\nDirecao: " + direction);
                 break;
             case R.id.joy2:
-                gameView.rotate(angle);
+                this.servo_pos = speed;
                 break;
         }
     }
-
-    @Override
-    public void onTap() { }
-
-    @Override
-    public void onDoubleTap() { }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
@@ -65,35 +71,38 @@ public class MainActivity extends AppCompatActivity implements JoyStick.JoyStick
                     Amarino.connect(getApplicationContext(), BLUETOOTH_MAC_ADDRESS);
                     return true;
                 case R.id.btn_a:
-                    textView.setText("acelerando");
+//                    textView.setText("acelerando");
+                    char flag = 'E';
+                    if (this.direction == JoyStick.DIRECTION_UP) {
+                        flag = 'A';
+                    } else if (this.direction == JoyStick.DIRECTION_DOWN) {
+                        flag = 'B';
+                    } else if (this.direction == JoyStick.DIRECTION_RIGHT) {
+                        flag = 'C';
+                    } else if (this.direction == JoyStick.DIRECTION_LEFT) {
+                        flag = 'D';
+                    }
+                    Amarino.sendDataToArduino(getApplicationContext(), BLUETOOTH_MAC_ADDRESS, flag, this.speed);
                     return true;
                 case R.id.btn_r1:
-                    Amarino.sendDataToArduino(getApplicationContext(), BLUETOOTH_MAC_ADDRESS, 'A', 1);
+                    Amarino.sendDataToArduino(getApplicationContext(), BLUETOOTH_MAC_ADDRESS, 'F', 1);
                     return true;
                 case R.id.btn_l1:
-                    Amarino.sendDataToArduino(getApplicationContext(), BLUETOOTH_MAC_ADDRESS, 'B', 1);
+                    Amarino.sendDataToArduino(getApplicationContext(), BLUETOOTH_MAC_ADDRESS, 'G', 1);
+                    return true;
+                case R.id.btn_y:
+                    textView.setText("servo: " + this.servo_pos);
                     return true;
             }
         } else if (action == MotionEvent.ACTION_UP) {
             switch(v.getId()) {
                 case R.id.btn_a:
-                    textView.setText("parou de acelerar");
+//                    textView.setText("parou de acelerar");
+                    Amarino.sendDataToArduino(getApplicationContext(), BLUETOOTH_MAC_ADDRESS, 'E', 0);
                     return true;
             }
         }
         return false;
-    }
-
-    public void getObject() {
-        JoyStick joyMove = (JoyStick) findViewById(R.id.joy1);
-        joyMove.setListener(this);
-        joyMove.setPadBackground(R.drawable.pad);
-        joyMove.setButtonDrawable(R.drawable.button);
-
-        JoyStick joyRotate = (JoyStick) findViewById(R.id.joy2);
-        joyRotate.setListener(this);
-        joyRotate.setPadBackground(R.drawable.pad);
-        joyRotate.setButtonDrawable(R.drawable.button);
     }
 
     @Override
