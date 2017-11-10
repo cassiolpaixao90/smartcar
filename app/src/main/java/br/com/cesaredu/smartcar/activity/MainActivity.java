05de7fcf7f3d6c35e7c8d36a56676f7bdaa522b4
Leonardo Trindade dos Santos Souza <leonardo-trindade@live.com>
2017-11-10 19:56:21 -0300

    toggle button fix

--- app/src/main/java/br/com/cesaredu/smartcar/activity/MainActivity.java
+++ app/src/main/java/br/com/cesaredu/smartcar/activity/MainActivity.java
@@ -7,9 +7,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
 import br.com.cesaredu.smartcar.broadcasts.AmarinoReceiver;
 import br.com.cesaredu.smartcar.utils.FlagsUtil;
@@ -24,7 +24,11 @@ import br.com.cesaredu.smartcar.R;
 public class MainActivity extends AppCompatActivity implements JoyStick.JoyStickListener, View.OnTouchListener, AmarinoReceiver.DistanceListener {
     public static final String BLUETOOTH_MAC_ADDRESS = "20:16:10:24:30:84";
     private TextView textView;
+    private Button r1;
+    private Button l1;
     private boolean togglePower = false;
+    private boolean toggleR1 = false;
+    private boolean toggleL1 = false;
     private int speed = 0;
     private int direction = -1;
 
@@ -33,127 +37,15 @@ public class MainActivity extends AppCompatActivity implements JoyStick.JoyStick
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textView);
-        findViewById(R.id.btn_a).setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (((ToggleButton) v).isChecked()) {
-                    findViewById(R.id.btn_a).setBackgroundResource(R.drawable.shapekeypress);
-                    @SuppressLint("WrongViewCast") GradientDrawable gd = (GradientDrawable) findViewById(R.id.btn_a).getBackground().getCurrent();
-                    gd.setColor(Color.parseColor("#191919"));
-                    gd.setCornerRadii(new float[]{100, 100, 100, 100, 100, 100, 100, 100});
-//                    gd.setStroke(2, Color.parseColor("#00FFFF"), 5, 6);
-
-                }else {
-                    findViewById(R.id.btn_a).setBackgroundResource(R.drawable.shape);
-                    @SuppressLint("WrongViewCast") GradientDrawable gd = (GradientDrawable) findViewById(R.id.btn_a).getBackground().getCurrent();
-                    gd.setColor(Color.parseColor("#000000"));
-                    gd.setCornerRadii(new float[]{100, 100, 100, 100, 100, 100, 100, 100});
-                }
-
-                return false;
-            }
-        });
-        findViewById(R.id.btn_b).setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (((ToggleButton) v).isChecked()) {
-                    findViewById(R.id.btn_b).setBackgroundResource(R.drawable.shapekeypress);
-                    @SuppressLint("WrongViewCast") GradientDrawable gd = (GradientDrawable) findViewById(R.id.btn_b).getBackground().getCurrent();
-                    gd.setColor(Color.parseColor("#191919"));
-                    gd.setCornerRadii(new float[]{100, 100, 100, 100, 100, 100, 100, 100});
-//                    gd.setStroke(2, Color.parseColor("#00FFFF"), 5, 6);
-
-                }else {
-                    findViewById(R.id.btn_b).setBackgroundResource(R.drawable.shape);
-                    @SuppressLint("WrongViewCast") GradientDrawable gd = (GradientDrawable) findViewById(R.id.btn_b).getBackground().getCurrent();
-                    gd.setColor(Color.parseColor("#000000"));
-                    gd.setCornerRadii(new float[]{100, 100, 100, 100, 100, 100, 100, 100});
-                }
-
-                return false;
-            }
-        });
-        findViewById(R.id.btn_l1).setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (((ToggleButton) v).isChecked()) {
-                    findViewById(R.id.btn_l1).setBackgroundResource(R.drawable.shapekeypress);
-                    @SuppressLint("WrongViewCast") GradientDrawable gd = (GradientDrawable) findViewById(R.id.btn_l1).getBackground().getCurrent();
-                    gd.setColor(Color.parseColor("#191919"));
-                    gd.setCornerRadii(new float[]{100, 100, 100, 100, 100, 100, 100, 100});
-//                    gd.setStroke(2, Color.parseColor("#00FFFF"), 5, 6);
-
-                }else {
-                    findViewById(R.id.btn_l1).setBackgroundResource(R.drawable.shape);
-                    @SuppressLint("WrongViewCast") GradientDrawable gd = (GradientDrawable) findViewById(R.id.btn_l1).getBackground().getCurrent();
-                    gd.setColor(Color.parseColor("#000000"));
-                    gd.setCornerRadii(new float[]{100, 100, 100, 100, 100, 100, 100, 100});
-                }
-
-                return false;
-            }
-        });
+        findViewById(R.id.btn_a).setOnTouchListener(this);
+        findViewById(R.id.btn_b).setOnTouchListener(this);
+        l1 = (Button) findViewById(R.id.btn_l1);
+        l1.setOnTouchListener(this);
         findViewById(R.id.btn_power).setOnTouchListener(this);
-        findViewById(R.id.btn_r1).setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (((ToggleButton) v).isChecked()) {
-                    findViewById(R.id.btn_r1).setBackgroundResource(R.drawable.shapekeypress);
-                    @SuppressLint("WrongViewCast") GradientDrawable gd = (GradientDrawable) findViewById(R.id.btn_r1).getBackground().getCurrent();
-                    gd.setColor(Color.parseColor("#191919"));
-                    gd.setCornerRadii(new float[]{100, 100, 100, 100, 100, 100, 100, 100});
-//                    gd.setStroke(2, Color.parseColor("#00FFFF"), 5, 6);
-
-                }else {
-                    findViewById(R.id.btn_r1).setBackgroundResource(R.drawable.shape);
-                    @SuppressLint("WrongViewCast") GradientDrawable gd = (GradientDrawable) findViewById(R.id.btn_r1).getBackground().getCurrent();
-                    gd.setColor(Color.parseColor("#000000"));
-                    gd.setCornerRadii(new float[]{100, 100, 100, 100, 100, 100, 100, 100});
-                }
-
-                return false;
-            }
-        });
-        findViewById(R.id.btn_x).setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (((ToggleButton) v).isChecked()) {
-                    findViewById(R.id.btn_x).setBackgroundResource(R.drawable.shapekeypress);
-                    @SuppressLint("WrongViewCast") GradientDrawable gd = (GradientDrawable) findViewById(R.id.btn_x).getBackground().getCurrent();
-                    gd.setColor(Color.parseColor("#191919"));
-                    gd.setCornerRadii(new float[]{100, 100, 100, 100, 100, 100, 100, 100});
-//                    gd.setStroke(2, Color.parseColor("#00FFFF"), 5, 6);
-
-                }else {
-                    findViewById(R.id.btn_l1).setBackgroundResource(R.drawable.shape);
-                    @SuppressLint("WrongViewCast") GradientDrawable gd = (GradientDrawable) findViewById(R.id.btn_x).getBackground().getCurrent();
-                    gd.setColor(Color.parseColor("#000000"));
-                    gd.setCornerRadii(new float[]{100, 100, 100, 100, 100, 100, 100, 100});
-                }
-
-                return false;
-            }
-        });
-        findViewById(R.id.btn_y).setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (((ToggleButton) v).isChecked()) {
-                    findViewById(R.id.btn_y).setBackgroundResource(R.drawable.shapekeypress);
-                    @SuppressLint("WrongViewCast") GradientDrawable gd = (GradientDrawable) findViewById(R.id.btn_y).getBackground().getCurrent();
-                    gd.setColor(Color.parseColor("#191919"));
-                    gd.setCornerRadii(new float[]{100, 100, 100, 100, 100, 100, 100, 100});
-//                    gd.setStroke(2, Color.parseColor("#00FFFF"), 5, 6);
-
-                }else {
-                    findViewById(R.id.btn_y).setBackgroundResource(R.drawable.shape);
-                    @SuppressLint("WrongViewCast") GradientDrawable gd = (GradientDrawable) findViewById(R.id.btn_y).getBackground().getCurrent();
-                    gd.setColor(Color.parseColor("#000000"));
-                    gd.setCornerRadii(new float[]{100, 100, 100, 100, 100, 100, 100, 100});
-                }
-
-                return false;
-            }
-        });
+        r1 = (Button) findViewById(R.id.btn_r1);
+        r1.setOnTouchListener(this);
+        findViewById(R.id.btn_x).setOnTouchListener(this);
+        findViewById(R.id.btn_y).setOnTouchListener(this);
         JoyStick joyMove = (JoyStick) findViewById(R.id.joy1);
         joyMove.setListener(this);
         joyMove.setPadBackground(R.drawable.pad);
@@ -222,9 +114,33 @@ public class MainActivity extends AppCompatActivity implements JoyStick.JoyStick
                     return true;
                 case R.id.btn_l1:
                     Amarino.sendDataToArduino(getApplicationContext(), BLUETOOTH_MAC_ADDRESS, FlagsUtil.LED_F, 1);
+                    toggleL1 = !toggleL1;
+                    if (toggleL1) {
+                        l1.setBackgroundResource(R.drawable.shapekeypress);
+                        @SuppressLint("WrongViewCast") GradientDrawable gd = (GradientDrawable) l1.getBackground().getCurrent();
+                        gd.setColor(Color.parseColor("#191919"));
+                        gd.setCornerRadii(new float[]{100, 100, 100, 100, 100, 100, 100, 100});
+                    } else {
+                        l1.setBackgroundResource(R.drawable.shape);
+                        @SuppressLint("WrongViewCast") GradientDrawable gd = (GradientDrawable) l1.getBackground().getCurrent();
+                        gd.setColor(Color.parseColor("#000000"));
+                        gd.setCornerRadii(new float[]{100, 100, 100, 100, 100, 100, 100, 100});
+                    }
                     return true;
                 case R.id.btn_r1:
                     Amarino.sendDataToArduino(getApplicationContext(), BLUETOOTH_MAC_ADDRESS, FlagsUtil.LED_T, 1);
+                    toggleR1 = !toggleR1;
+                    if (toggleR1) {
+                        r1.setBackgroundResource(R.drawable.shapekeypress);
+                        @SuppressLint("WrongViewCast") GradientDrawable gd = (GradientDrawable) r1.getBackground().getCurrent();
+                        gd.setColor(Color.parseColor("#191919"));
+                        gd.setCornerRadii(new float[]{100, 100, 100, 100, 100, 100, 100, 100});
+                    } else {
+                        r1.setBackgroundResource(R.drawable.shape);
+                        @SuppressLint("WrongViewCast") GradientDrawable gd = (GradientDrawable) r1.getBackground().getCurrent();
+                        gd.setColor(Color.parseColor("#000000"));
+                        gd.setCornerRadii(new float[]{100, 100, 100, 100, 100, 100, 100, 100});
+                    }
                     return true;
                 case R.id.btn_x:
                     Amarino.sendDataToArduino(getApplicationContext(), BLUETOOTH_MAC_ADDRESS, FlagsUtil.BUZ, 1);
